5b081b2c64c8f8a8a5de73cd25262521235f3d00
Terry Young <terryyounghk@gmail.com>
2023-02-16 19:27:11 +0800

    change caps sounds

--- user_song_list.h
+++ user_song_list.h
@@ -1,8 +1,6 @@
 // https://docs.qmk.fm/#/feature_audio?id=songs
 
 #define TY_LAYER_SOUND          S__NOTE(_C6), S__NOTE(_E6), S__NOTE(_G6), S__NOTE(_REST)
-#define TY_CAPS_ON_SOUND        S__NOTE(_C6), S__NOTE(_E6), S__NOTE(_G6), H__NOTE(_C7)
-#define TY_CAPS_OFF_SOUND       S__NOTE(_C7), S__NOTE(_G6), S__NOTE(_E6), H__NOTE(_C6)
 
 #define TY_ALPHAS_SOUND         TY_LAYER_SOUND
 #define TY_NUMBERS_SOUND        TY_LAYER_SOUND, TY_LAYER_SOUND
@@ -13,8 +11,8 @@
 #define TY_OSM_OFF_SOUND        S__NOTE(_D5), S__NOTE(_D5)
 
 // alternatively, apps like Captin give both visual and audio feedback for Caps Lock
-#define TY_CAPS_LOCK_ON_SOUND   TY_CAPS_ON_SOUND,  TY_CAPS_ON_SOUND,  TY_CAPS_ON_SOUND
-#define TY_CAPS_LOCK_OFF_SOUND  TY_CAPS_OFF_SOUND, TY_CAPS_OFF_SOUND, TY_CAPS_OFF_SOUND
+#define TY_CAPS_LOCK_ON_SOUND   CAPS_LOCK_ON_SOUND
+#define TY_CAPS_LOCK_OFF_SOUND  CAPS_LOCK_OFF_SOUND
 
-#define TY_CAPS_WORD_ON_SOUND   TY_CAPS_ON_SOUND
-#define TY_CAPS_WORD_OFF_SOUND  TY_CAPS_OFF_SOUND
+#define TY_CAPS_WORD_ON_SOUND   S__NOTE(_E7), S__NOTE(_C7), S__NOTE(_E7), E__NOTE(_C7)
+#define TY_CAPS_WORD_OFF_SOUND  S__NOTE(_B6), S__NOTE(_E7), S__NOTE(_B6), E__NOTE(_E7)
